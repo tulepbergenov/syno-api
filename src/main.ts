d43b883e1bcbae07c7b1f7beb6c101e04044b359
Daulet Tulepbergenov <ilovepixiess@gmail.com>
2026-04-21 21:23:45 +0500

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,22 @@ import { ValidationPipe } from "@nestjs/common";
 import "dotenv/config";
 import { Reflector } from "@nestjs/core";
 import { JwtAuthGuard } from "./common/guards/jwt-auth.guard";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle("Cats example")
+    .setDescription("The cats API description")
+    .setVersion("1.0")
+    .addTag("cats")
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api/docs", app, documentFactory, {
+    jsonDocumentUrl: "api/docs/json",
+  });
 
   const reflector = app.get(Reflector);
 
@@ -16,6 +29,7 @@ async function bootstrap() {
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.useGlobalInterceptors(new ResponseInterceptor());
   app.useGlobalFilters(new HttpExceptionFilter());
+  app.setGlobalPrefix("api");
 
   await app.listen(process.env.PORT ?? 3000);
 }
