f304733a05faa6d07c86be22b50c23f637f446d6
Daulet Tulepbergenov <ilovepixiess@gmail.com>
2026-05-07 01:31:56 +0500

    fix: up import style and enforce envs vars usage

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { PrismaModule } from "@core/prisma/prisma.module";
 import { UsersModule } from "@modules/users/users.module";
 import { AuthModule } from "@modules/auth/auth.module";
 import { envSchema } from "@core/config/env.validation";
-import { MeModule } from './modules/me/me.module';
+import { MeModule } from "./modules/me/me.module";
 
 @Module({
   imports: [
--- src/core/config/minio.config.ts
+++ src/core/config/minio.config.ts
@@ -2,10 +2,10 @@ import { Client } from "minio";
 
 export function createMinioClient(): Client {
   return new Client({
-    endPoint: process.env.MINIO_ENDPOINT || "localhost",
-    port: parseInt(process.env.MINIO_PORT || "9000"),
+    endPoint: process.env.MINIO_ENDPOINT!,
+    port: parseInt(process.env.MINIO_PORT!),
     useSSL: process.env.MINIO_USE_SSL === "true",
-    accessKey: process.env.MINIO_ROOT_USER || "minioadmin",
-    secretKey: process.env.MINIO_ROOT_PASSWORD || "minioadmin",
+    accessKey: process.env.MINIO_ROOT_USER!,
+    secretKey: process.env.MINIO_ROOT_PASSWORD!,
   });
 }
